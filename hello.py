ceaa48c0a09957d17941a20dfb96a4c0f50fc9f3
feliplourenco <felipecunegundes@outlook.com>
2025-01-17 00:52:12 +0000

    Alterações para prova 3

--- hello.py
+++ hello.py
@@ -55,16 +55,6 @@ def make_shell_context():
     return dict(db=db, User=User, Role=Role)
 
 
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
-
-
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template('500.html'), 500
-
 @app.route('/indisponivel')
 def indisponivel():
   return render_template('indisponivel.html', current_time=datetime.utcnow())
